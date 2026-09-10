91ae3bddbbe4fa05990cdacca8d24f3e29c94567
Opilane Techno+TLN <aron.randmae@gmail.com>
2026-09-10 10:52:13 +0300

    IfAndElseNumbers

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2026");
+
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisestatud väärtus on number
+            //kasutada if ja else lauseid
+
+            string number = Console.ReadLine();
+
+            if (number == "")
+            {
+                
+            }
+
+            Console.WriteLine("sisestasid numbri");
+            Console.WriteLine(number);
+
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            
+        }
+    }
+}
