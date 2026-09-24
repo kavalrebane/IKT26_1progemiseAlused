1d2b6a304f5d089bcf05292db92fce1ec5afa5ac
Opilane Techno+TLN <aron.randmae@gmail.com>
2026-09-24 09:28:08 +0300

    IfElseColour

---
+++ 24.09.26/IfElseColour/IfElseColour/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace IfElseColour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha if ja else konsoolirakendus, kus " +
+                "kontrollitakse stringi abil värvi vastavust");
+
+            Console.WriteLine("Värvide valikus on: red, blue, green ja white");
+
+            Console.WriteLine("Peab käsitlema juhust, kus vastaja ei sisesta " +
+                "eelpool sisestatud värvi");
+
+            Console.WriteLine("Sisestage värv");
+            //kirjuta konsooli tekst ja vajuta enter
+            string color = Console.ReadLine();
+
+            //see kontrollib, kas on punane värv
+            //kui kasutad if ja else, siis esimene kontroll on if
+            //ja järgneb else if ning kõige viimane on alati else
+            if (color == "red")
+            {
+                Console.WriteLine("See värv on punane");
+            }
+            else if (color == "blue")
+            {
+                Console.WriteLine("See värv on sinine");
+            }
+            else if (color == "green")
+            {
+                Console.WriteLine("See värv on roheline");
+            }
+            else if (color == "white")
+            {
+                Console.WriteLine("See värv on valge");
+            }
+            else
+            {
+                Console.WriteLine("See värv on jama");
+            }
+        }
+    }
+}
